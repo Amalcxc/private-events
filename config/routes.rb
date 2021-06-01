72b0409dd27a4f8dfd394b123445208f51b172f9
Amalcxc <amalcxirsi@gmail.com>
2021-06-01 23:19:32 +0300

    Separate this list of “attended_events”

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #resources :events
+  resources :users, only: [:index, :show]
   resources :events 
+  resources :lists do
+    member do
+      post :attend
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   match '/all_events', to: 'events#index', via: :get
-  post 'attend_list', to: 'lists#attend', as: 'attend'
 end
